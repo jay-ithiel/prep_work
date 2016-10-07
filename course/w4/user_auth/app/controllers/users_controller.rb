f62c48814681bdd392fdfa70a9bf60d468590c5a
Jay Hwang <jayhwang0121@gmail.com>
2016-10-06 22:36:47 -0700

    create user table and model

--- course/w4/user_auth/app/controllers/users_controller.rb
+++ course/w4/user_auth/app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
   def index
-
+    render :index
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :password)
+    end
 end
