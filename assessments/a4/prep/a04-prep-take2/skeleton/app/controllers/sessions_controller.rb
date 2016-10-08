a82a5f2ddf86215be26ea447a448cb36e2d8e4c5
Jay Hwang <jayhwang0121@gmail.com>
2016-10-08 16:38:47 -0700

    add sessions controller and sign in form

--- assessments/a4/prep/a04-prep-take2/skeleton/app/controllers/sessions_controller.rb
+++ assessments/a4/prep/a04-prep-take2/skeleton/app/controllers/sessions_controller.rb
@@ -3,9 +3,22 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
 
+    if user.nil?
+      flash.now[:errors] = ["Invalid credentials"]
+      render :new
+    else
+      login!(user)
+      redirect_to links_url
+    end
   end
 
   def destroy
+    logout!
+    redirect_to new_session_url
   end
 end
