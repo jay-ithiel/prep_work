37752b4b3e56d5435b63c50001bfc791d1bbf396
Jay Hwang <jayhwang0121@gmail.com>
2016-10-07 22:44:21 -0700

    add subs controller actions

--- course/w4/w4d5/reddit2/app/controllers/subs_controller.rb
+++ course/w4/w4d5/reddit2/app/controllers/subs_controller.rb
@@ -1,20 +1,41 @@
 class SubsController < ApplicationController
   def new
+    @sub = Sub.new
+    render :new
   end
 
   def create
+    @sub = Sub.new(sub_params)
+    if @sub.save
+      redirect_to sub_url(@sub)
+    else
+      flash.now[:errors] = @sub.errors.full_messages
+      render :new
+    end
   end
 
   def edit
+    @sub = Sub.find(params[:id])
+    render :edit
   end
 
   def update
+    @sub = Sub.find(params[:id])
+    if @sub.update(sub_params)
+      redirect_to sub_url(@sub)
+    else
+      flash.now[:errors] = @sub.errors.full_messages
+      render :edit
+    end
   end
 
   def show
+    @sub = Sub.find(params[:id])
+    render :show
   end
 
   def index
+    @subs = Sub.all
   end
 
   def destroy
