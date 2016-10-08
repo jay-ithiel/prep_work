cf0fb5c90a56b5882eb727366e262085693f3de7
Jay Hwang <jayhwang0121@gmail.com>
2016-10-07 20:10:53 -0700

    user_auth new

--- course/w4/user_auth/app/controllers/application_controller.rb
+++ course/w4/user_auth/app/controllers/application_controller.rb
@@ -1,14 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def login_user!(user)
-    user.reset_session_token!
-    session[:session_token] = user.session_token
-    redirect_to user_url(user)
-  end
-
-  def current_user
-    return nil unless session[:session_token]
-    @current_user ||= User.find_by_session_token(session[:session_token])
-  end
 end
