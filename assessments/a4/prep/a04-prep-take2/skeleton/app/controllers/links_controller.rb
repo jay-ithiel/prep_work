c294f7077d3ee55fc94f75aacdf5ba1e0d1f703f
Jay Hwang <jayhwang0121@gmail.com>
2016-10-08 16:41:53 -0700

    add links controller actions

--- assessments/a4/prep/a04-prep-take2/skeleton/app/controllers/links_controller.rb
+++ assessments/a4/prep/a04-prep-take2/skeleton/app/controllers/links_controller.rb
@@ -1,2 +1,29 @@
 class LinksController < ApplicationController
+
+  def new
+  end
+
+  def create
+    
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+
+
+  private
+
+    def link_params
+      params.require(:link).permit(:title, :url)
+    end
 end
