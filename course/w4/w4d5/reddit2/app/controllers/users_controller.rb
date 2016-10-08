4c1287a9dade482e6a0dd75a7f8b87b76e2c3f1b
Jay Hwang <jayhwang0121@gmail.com>
2016-10-07 21:44:37 -0700

    add user model functionality

--- course/w4/w4d5/reddit2/app/controllers/users_controller.rb
+++ course/w4/w4d5/reddit2/app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+  def new
+    render :new
+  end
+
+  def create
+    @user = User.find(params[:id])
+    if @user.save
+      
+    else
+
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :password)
+    end
 end
