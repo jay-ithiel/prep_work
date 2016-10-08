189e9a5ce7523272e6c55f061447245dc5cb2666
Jay Hwang <jayhwang0121@gmail.com>
2016-10-07 21:50:47 -0700

    add session_controller actions

--- course/w4/w4d5/reddit2/app/controllers/sessions_controller.rb
+++ course/w4/w4d5/reddit2/app/controllers/sessions_controller.rb
@@ -1,3 +1,24 @@
 class SessionsController < ApplicationController
-  
+  def new
+    render :new
+  end
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    @user.reset_session_token!
+    session[:session_token] = @user.session_token
+
+    redirect_to :user_url(@user)
+  end
+
+  def destroy
+    @user.reset_session_token!
+    session[:session_token] = nil
+
+    redirect_to new_session_url
+  end
 end
