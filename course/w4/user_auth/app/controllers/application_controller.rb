200fc175b6f26726e6558aef6af1283af9dbbcf1
Jay Hwang <jayhwang0121@gmail.com>
2016-10-07 19:26:13 -0700

    mod controllers and user model

--- course/w4/user_auth/app/controllers/application_controller.rb
+++ course/w4/user_auth/app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  login_user!(user)
-    # handle user login 
+  def login_user!(user)
+    user.reset_session_token!
+    session[:session_token] = user.session_token
+    redirect_to user_url(user)
+  end
+
+  def current_user
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by_session_token(session[:session_token])
   end
 end
--- course/w4/user_auth/app/models/user.rb
+++ course/w4/user_auth/app/models/user.rb
@@ -19,7 +19,7 @@ class User < ApplicationRecord
     self.session_token ||= User.gen_session_token
   end
 
-  def reset_session_token
+  def reset_session_token!
     self.session_token = User.gen_session_token
     self.save
   end
