5da2957b9d58a2a3fd200c8c38c3bb3782cfb729
Jay Hwang <jayhwang0121@gmail.com>
2016-10-09 22:39:36 -0700

    end of day commit

--- assessments/a4/prep/a04-prep-2/skeleton/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Links::Application.routes.draw do
-  resources :users, only: [:new, :create]
-  resources :links
-  resource :session, only: [:new, :create, :destroy]
-  resources :comments, only: [:create, :destroy]
-end
