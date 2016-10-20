621f707a8c39f584e93d6279cee05a7eeb03237d
Jay Hwang <jayhwang0121@gmail.com>
2016-10-19 22:13:21 -0700

    prac

--- assessments/a5/prep/prac/src/js_assessment.js
+++ assessments/a5/prep/prac/src/js_assessment.js
@@ -1,6 +1,5 @@
 // write String.prototype.mySlice. It should take a start index and an
 // (optional) end index.
-
 String.prototype.mySlice = function(startIdx, endIdx) {
   if (endIdx == null || endIdx > this.length) { endIdx = this.length }
   let sliced = "";
@@ -13,15 +12,15 @@ String.prototype.mySlice = function(startIdx, endIdx) {
 }
 
 // write Array.prototype.myReduce (analogous to Ruby's Array#inject).
-
 Array.prototype.myReduce = function(callback, accumulator) {
   if (accumulator == null) { accumulator = this.shift(); }
-  for (let i = 0; i < this.length; i++) {
-    accumulator = callback(accumulator, this[i]);
-  }
+
+  this.forEach(function(el) {
+    accumulator = callback(accumulator, el);
+  });
+
   return accumulator;
 }
-
 
 // write Array.prototype.quickSort(comparator). Here's a quick refresher if
 // you've forgotten how quickSort works:
@@ -33,86 +32,82 @@ Array.prototype.myReduce = function(callback, accumulator) {
 //   - recursively call quickSort on the left and right halves, and return the
 //   full sorted array.
 
+
 Array.prototype.quickSort = function(comparator) {
   if (this.length <= 1) { return this }
 
-  if (comparator == null) { comparator = (x, y) => {
-    if (x === y) {
-      return 0;
-    } else if (x < y) {
-      return -1;
-    } else {
-      return 1;
+  if (typeof(comparator) != 'function') {
+    comparator = (a, b) => {
+      if (a < b) {
+        return -1;
+      } else if (a === b) {
+        return 0;
+      } else {
+        return 1;
+      }
     }
-  }}
+  }
 
-  let pivot = this[0];
+  const pivot = this[0];
   let left = [];
   let right = [];
 
   for (let i = 1; i < this.length; i++) {
-    let curr = this[i];
-    if (comparator(pivot, curr) < 0) {
-      right.push(curr);
+    if (comparator(this[i], pivot) < 0) {
+      left.push(this[i]);
     } else {
-      left.push(curr);
+      right.push(this[i]);
     }
   }
 
-  let leftSorted = left.quickSort();
-  let rightSorted = right.quickSort();
+
+  let leftSorted = left.quickSort(comparator);
+  let rightSorted = right.quickSort(comparator);
 
   return leftSorted.concat(pivot).concat(rightSorted);
 }
 
-
-// write myFind(array, callback). It should return the first element for
-// which callback returns true, or undefined if none is found.
+// write myFind(array, callback). It should return the first element for which
+// callback returns true, or undefined if none is found.
 function myFind(array, callback) {
   for (let i = 0; i < array.length; i++) {
-    if (callback(array[i])) { return array[i] }
+    let curr = array[i];
+    if (callback(curr)) { return curr }
   }
   return undefined;
 }
 
+
 // write sumNPrimes(n)
 function isPrime(n) {
-  if (n < 2) { return false }
-  for (let div = 2; div < n; div++) {
+  if (n < 2) { return 0 }
+  for (div = 2; div < n; div++) {
     if (n % div == 0) { return false }
   }
   return true;
 }
 
-
 function sumNPrimes(n) {
-  if (n === 0) { return 0 }
+  if (n == 0) { return 0 }
   let nPrimes = [];
 
-  for (let num = 2; nPrimes.length < n; num++) {
-    if (isPrime(num)) { nPrimes.push(num) }
+  for (let num = 0; nPrimes.length < n; num++) {
+    if (isPrime(num)) { nPrimes.push(num); }
   }
 
-  return nPrimes.myReduce((a,b) => a + b );
+  return nPrimes.myReduce((a, b) => a + b);
 }
-
-
 
 // write Function.prototype.myBind.
-Function.prototype.myBind = function(context, arg) {
-  let args = Array.from(arguments);
-  alert(arg.name);
-  if (args.length > 1) {
-    return () => { return this.apply(context, args.splice(1)) }
-  } else {
-    return () => { return this.apply(context)}
+Function.prototype.myBind = function(context, ...bindArgs) {
+  return (...arguments) => {
+    return this.apply(context,  bindArgs.concat(arguments));
   }
 }
-
 
 // write Function.prototype.inherits.
 Function.prototype.inherits = function(Parent) {
-  function Surrogate() {}
+  function Surrogate() {};
   Surrogate.prototype = Parent.prototype;
   this.prototype = new Surrogate();
   this.prototype.constructor = this;
