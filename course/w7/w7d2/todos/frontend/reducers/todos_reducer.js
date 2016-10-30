c83b9167432a0dd8b6fc05e9b1b820fe55fa3988
Jay Hwang <jayhwang0121@gmail.com>
2016-10-30 14:51:21 -0700

    create todos_reducer and store

---
+++ course/w7/w7d2/todos/frontend/reducers/todos_reducer.js
@@ -0,0 +1,40 @@
+import {
+  REQUEST_TODOS,
+  RECEIVE_TODOS
+} from '../actions/todo_actions.js';
+
+const _defaultState = {
+  '1': {
+    id: 1,
+    title: "wash car",
+    body: "with soap",
+    done: false
+  },
+  '2': {
+    id: 2,
+    title: "wash dog",
+    body: "with shampoo",
+    done: true
+  },
+};
+
+const TodosReducer = (state = _defaultState, action) => {
+  Object.freeze(state);
+
+  switch(action.type) {
+    case REQUEST_TODOS:
+      return state;
+
+    case RECEIVE_TODOS:
+      let newState = {};
+      action.todos.forEach(todo => {
+        newState[todo.id] = todo;
+      });
+      return newState;
+
+    default:
+      return state;
+  }
+};
+
+export default TodosReducer;
--- course/w7/w7d2/todos/frontend/todo_redux.jsx
+++ course/w7/w7d2/todos/frontend/todo_redux.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
   const rootContent = document.getElementById("content");
+  const store = configureStore();
+
   ReactDOM.render(<h1>Todos App</h1>, rootContent);
+
+  window.store = store;
 });
