f01485df087faec5dff6392dfd563876b9a33ed8
Jay Hwang <jayhwang0121@gmail.com>
2016-10-30 14:17:41 -0700

    phase 1

---
+++ course/w7/w7d2/todos/frontend/todo_redux.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const rootContent = document.getElementById("content");
+  ReactDOM.render(<h1>Todos App</h1>, rootContent);
+});
